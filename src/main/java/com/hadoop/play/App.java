b4d309b2e4aa5e6b90c9ebd6f7731254c3a989a2
Srinivas Kancharla1 <getreddy@gmail.com>
2014-11-18 21:35:40 -0800

    message

--- src/main/java/com/hadoop/play/App.java
+++ src/main/java/com/hadoop/play/App.java
@@ -1,13 +1,13 @@
 package com.hadoop.play;
 
 /**
- * Hello world!
+ * Hadoop cluster write test
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Writing and Reading to hadoop cluster done.." );
     }
 }
